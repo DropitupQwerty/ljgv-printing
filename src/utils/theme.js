df1c1c37bfe33c4f7ec3fb5e63fb28ae8444a33c
DropitupQwerty <vjacoballen3020@gmail.com>
2022-05-18 09:26:22 +0800

    Background color to white

--- src/utils/theme.js
+++ src/utils/theme.js
@@ -1,37 +1,37 @@
-import { createTheme } from "@mui/material";
+import { createTheme } from '@mui/material';
 
 const theme = createTheme({
   palette: {
-    mode: "light",
+    mode: 'light',
     primary: {
-      main: "#5D3FD3",
+      main: '#5D3FD3',
     },
     secondary: {
-      main: "#333",
+      main: '#333',
     },
     dashboard: {
-      bg1: "#947EC3",
-      bg2: "#B689C0",
-      bg3: "#EC994B",
+      bg1: '#fffff',
+      bg2: '#B689C0',
+      bg3: '#EC994B',
     },
     text: {
-      t1: "#333",
-      t2: "#fff",
+      t1: '#333',
+      t2: '#fff',
     },
   },
   typography: {
     fontFamily: [
-      "-apple-system",
-      "BlinkMacSystemFont",
-      "Segoe UI",
-      "Roboto",
-      "Oxygen",
-      "Ubuntu",
-      "Cantarell",
-      "Open Sans",
-      "Helvetica Neue",
-      "sans-serif",
-    ].join(","),
+      '-apple-system',
+      'BlinkMacSystemFont',
+      'Segoe UI',
+      'Roboto',
+      'Oxygen',
+      'Ubuntu',
+      'Cantarell',
+      'Open Sans',
+      'Helvetica Neue',
+      'sans-serif',
+    ].join(','),
   },
 });
 
